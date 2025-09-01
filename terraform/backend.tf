ccdf1c65c6661e108b44cf0151b7c6e537efee22
Tej Patel <tpj7911@gmail.com>
2025-09-01 13:33:23 -0400

    editted backend.tf file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,36 +1,36 @@
-resource "aws_s3_bucket" "backend-bucket" {
-    bucket = "java-app-iac-backend-bucket"
-}
+# resource "aws_s3_bucket" "backend-bucket" {
+#     bucket = "java-app-iac-backend-bucket"
+# }
 
-resource "aws_s3_bucket_versioning" "backend-bucket-versioning-policy" {
-  bucket = aws_s3_bucket.backend-bucket.id
-  versioning_configuration {
-    status     = "Enabled"
-    mfa_delete = "Disabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "backend-bucket-versioning-policy" {
+#   bucket = aws_s3_bucket.backend-bucket.id
+#   versioning_configuration {
+#     status     = "Enabled"
+#     mfa_delete = "Disabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "backend-bucket-encryption-policy" {
-  bucket = aws_s3_bucket.backend-bucket.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "backend-bucket-encryption-policy" {
+#   bucket = aws_s3_bucket.backend-bucket.id
 
-  rule {
+#   rule {
 
-    bucket_key_enabled = false
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#     bucket_key_enabled = false
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-resource "aws_dynamodb_table" "backend-table" {
-  name = "java-app-backend-state-lock"
-  hash_key = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "backend-table" {
+#   name = "java-app-backend-state-lock"
+#   hash_key = "LockID"
+#   billing_mode = "PAY_PER_REQUEST"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 
 terraform {
   backend "s3" {
