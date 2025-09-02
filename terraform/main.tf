dc8ac9c0e82b5a0e65c5be54c44e7df8431b0f04
Tej Patel <tpj7911@gmail.com>
2025-09-02 16:08:59 -0400

    added launch template and auto scaling group.

--- terraform/main.tf
+++ terraform/main.tf
@@ -242,5 +242,5 @@ module "autoscaling_group" {
     module.private_subnet_2.subnet_id,
     module.private_subnet_3.subnet_id]
   ami_id = module.jump_start_server.ami_id
-  vpc_security_group_ids = [module.public_security_group]
+  vpc_security_group_ids = [module.public_security_group.sg_id]
 }
