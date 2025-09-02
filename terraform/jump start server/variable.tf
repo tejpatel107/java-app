711858931305d21dbdd66536f3c0d9cf4f135845
Tej Patel <tpj7911@gmail.com>
2025-09-02 15:02:40 -0400

    fixed user data.

--- terraform/jump start server/variable.tf
+++ terraform/jump start server/variable.tf
@@ -42,41 +42,49 @@ variable "user_data" {
   type        = string
   default     = <<-EOF
                 #!/bin/bash
-    set -xe
-    sudo yum update -y
-    sudo yum install -y java-17-amazon-corretto-headless awscli
-    sudo dnf install -y postgresql17
-    sudo aws s3 cp s3://java-app-jar-files-bucket/jar/demo-0.0.1-SNAPSHOT.jar /home/ec2-user/app/java-app.jar
-    sudo chown ec2-user:ec2-user /home/ec2-user/app/java-app.jar
 
-    sudo tee /etc/java-app.env > /dev/null <<'EOL'
-    DB_HOST=java-app-postgres.c0l8oguqa1lb.us-east-1.rds.amazonaws.com
-    DB_PORT=5432
-    DB_NAME=postgres
-    DB_USERNAME=postgres
-    DB_PASSWORD=java-postgres
-    EOL
 
-    sudo tee /etc/systemd/system/java-app.service > /dev/null <<EOL
-    [Unit]
-    Description=Spring Boot Application
-    After=network.target
+                # Make user-data re-run on every restart
+                sudo cloud-init clean
+                echo "true" > /var/lib/cloud/instance/sem/config_scripts_user
 
-    [Service]
-    User=ec2-user
-    EnvironmentFile=/etc/java-app.env
-    ExecStart=/usr/bin/java -jar /home/ec2-user/java-app.jar
-    Restart=always
-    RestartSec=5
-    StandardOutput=file:/home/ec2-user/app.log
-    StandardError=file:/home/ec2-user/app-error.log
+                set -xe
+                sudo yum update -y
+                sudo yum install -y java-17-amazon-corretto-headless awscli
+                sudo dnf install -y postgresql17
+                sudo mkdir app
+                sudo aws s3 cp s3://java-app-jar-files-bucket/jars/demo-0.0.1-SNAPSHOT.jar /home/ec2-user/app/java-app.jar
+                sudo chown ec2-user:ec2-user /home/ec2-user/app/java-app.jar
 
-    [Install]
-    WantedBy=multi-user.target
-    EOL
-    sudo systemctl daemon-reload
-    sudo systemctl enable java-app
-    sudo systemctl start java-app
+                sudo tee /etc/java-app.env > /dev/null <<'EOL'
+                DB_HOST=java-app-postgres.c0l8oguqa1lb.us-east-1.rds.amazonaws.com
+                DB_PORT=5432
+                DB_NAME=postgres
+                DB_USERNAME=postgres
+                DB_PASSWORD=java-postgres
+                EOL
 
+                sudo tee /etc/systemd/system/java-app.service > /dev/null <<EOL
+                [Unit]
+                Description=Spring Boot Application
+                After=network.target
+
+                [Service]
+                User=ec2-user
+                EnvironmentFile=/etc/java-app.env
+                ExecStart=/usr/bin/java -jar /home/ec2-user/java-app.jar
+                Restart=always
+                RestartSec=5
+                StandardOutput=file:/home/ec2-user/app.log
+                StandardError=file:/home/ec2-user/app-error.log
+
+                [Install]
+                WantedBy=multi-user.target
+                EOL
+                
+                sudo systemctl daemon-reload
+                sudo systemctl enable java-app
+                sudo systemctl start java-app
+      
   EOF
 }
